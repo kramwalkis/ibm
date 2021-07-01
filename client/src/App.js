7cff760697ddab4caea77e39918ab4e5e056d746
kramwalkis <julius.kramwalkis@gmail.com>
2021-07-01 21:59:16 +0300

    container with articles added and scroll to bottom event triggeered

--- client/src/App.js
+++ client/src/App.js
@@ -6,35 +6,52 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import InputGroup from "react-bootstrap/InputGroup";
+import Card from "react-bootstrap/Card";
 
 function App() {
   const searchRef = useRef();
-  const string = "";
-  const [lastArticleDate, setLastArticleDate] = useState({ date: string });
+  const scrollRef = useRef();
+  const [lastArticleDate, setLastArticleDate] = useState({ date: "" });
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
     searchRef.current.focus();
   }, []);
 
-  
+  function dateConverter(date) {
+    const regularDate = new Date(date);
+    let returnDate = `${regularDate.getFullYear()}-${
+      regularDate.getMonth() + 1
+    }-${regularDate.getDate()} ${regularDate.getHours()}:${regularDate.getMinutes()}:${regularDate.getSeconds()}`;
+    return returnDate;
+  }
+
   async function fetchFromClick(searchQuery) {
     const request = await fetch(
-      `https://gnews.io/api/v4/search?q=${searchQuery}&max=9&&token=f761bc3b88119973046aad53fa4bd099`
+      `https://gnews.io/api/v4/search?q=${searchQuery}&max=9&token=bb9ec8144134db3ca00bcdd567945b9a`
     );
     const data = await request.json();
     console.log(data);
-    setArticles([...data.articles])
+    setArticles([...data.articles]);
     setLastArticleDate({
       ...{ date: data.articles[data.articles.length - 1].publishedAt },
     });
   }
 
-  function logArticles () {
+  function logArticles() {
     console.log(articles);
   }
+
+  function scrollLogic() {
+    if (scrollRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = scrollRef.current;
+      if (Math.floor(scrollTop) + clientHeight === scrollHeight) {
+        console.log("youhave reached the bottom");
+      }
+    }
+  }
   return (
-    <div className="App">
+    <div className="App" onScroll={scrollLogic} ref={scrollRef}>
       <InputGroup>
         <FormControl ref={searchRef} />
         <InputGroup.Append>
@@ -44,10 +61,29 @@ function App() {
           >
             Button
           </Button>
-          <Button onClick={logArticles} >log articles</Button>
+          <Button onClick={logArticles}>log articles</Button>
         </InputGroup.Append>
       </InputGroup>
-      {/* <h1>{lastArticleDate.date}</h1> */}
+      <Container className="mt-3 mb-0 pb-0" fluid>
+        <Row>
+          {articles.map((article, index) => {
+            return (
+              <Col key={index} xs={4}>
+                <Card className="m-1">
+                  <Card.Img variant="top" src={article.image} />
+                  <Card.Body>
+                    <Card.Title>{article.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {dateConverter(article.publishedAt)}
+                    </Card.Subtitle>
+                    <Card.Text>{article.description}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }

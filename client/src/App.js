b0cf4d9b55bf6666fa2642f575789e3fa07f59e4
kramwalkis <julius.kramwalkis@gmail.com>
2021-07-01 16:41:57 +0300

    first fetch from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,53 @@
 import "./App.scss";
+import { useRef, useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
-
+import FormControl from "react-bootstrap/FormControl";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function App() {
-  
-  
+  const searchRef = useRef();
+  const string = "";
+  const [lastArticleDate, setLastArticleDate] = useState({ date: string });
+  const [articles, setArticles] = useState([]);
 
-  const fetchItems = async () => {
-    const data = await fetch("http://localhost:4000/testdata");
-    const items = await data.json();
-   console.log(items);
-   
-  };
+  useEffect(() => {
+    searchRef.current.focus();
+  }, []);
+
+  
+  async function fetchFromClick(searchQuery) {
+    const request = await fetch(
+      `https://gnews.io/api/v4/search?q=${searchQuery}&max=9&&token=f761bc3b88119973046aad53fa4bd099`
+    );
+    const data = await request.json();
+    console.log(data);
+    setArticles([...data.articles])
+    setLastArticleDate({
+      ...{ date: data.articles[data.articles.length - 1].publishedAt },
+    });
+  }
+
+  function logArticles () {
+    console.log(articles);
+  }
   return (
-    <div>
-      Hello from IBM task
-      <Button onClick={fetchItems} variant="danger">IBM</Button>      
+    <div className="App">
+      <InputGroup>
+        <FormControl ref={searchRef} />
+        <InputGroup.Append>
+          <Button
+            onClick={() => fetchFromClick(searchRef.current.value)}
+            variant="outline-secondary"
+          >
+            Button
+          </Button>
+          <Button onClick={logArticles} >log articles</Button>
+        </InputGroup.Append>
+      </InputGroup>
+      {/* <h1>{lastArticleDate.date}</h1> */}
     </div>
   );
 }

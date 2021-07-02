aa0c2cff629498ab1c7d624cf97d49ce45532b35
kramwalkis <julius.kramwalkis@gmail.com>
2021-07-02 11:50:55 +0300

    front side connection with back established

--- client/src/App.js
+++ client/src/App.js
@@ -65,6 +65,20 @@ function App() {
   }
 
   function noErrorsOnClick(input) {
+    let data = {
+      body: input,
+    };
+    fetch("/addSearchQuery", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((info) => {
+        console.log(info);
+      });
     document.getElementById("inputErrorMsg").style.display = "none";
     fetchFromClick(input);
   }

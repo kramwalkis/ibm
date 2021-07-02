8dbae18012dcfe81cd74d921441edb0fff4c9d65
kramwalkis <julius.kramwalkis@gmail.com>
2021-07-02 14:45:10 +0300

    search query adds to database

--- server/models/main.js
+++ server/models/main.js
@@ -1,6 +1,14 @@
+const queryDb = require("../schemas/querySchema");
+
 module.exports = {
-    addSearchQuery: (req, res) => {        
-        console.log(req.body);
-        res.send({ success: true, error: false });        
-    }
-}
+  addSearchQuery: async (req, res) => {
+    const queryFromFront = req.body.query;
+    const query = new queryDb();
+    query.query = queryFromFront;
+    await query.save();
+    const test = await queryDb.find({ query: queryFromFront });
+    test.length > 0
+      ? res.send({ success: true, error: false })
+      : res.send({ success: true, error: "something went wrong" });
+  },
+};

f03cb41deb9b93ee976be9b29de4a6c5fd4ec423
kramwalkis <julius.kramwalkis@gmail.com>
2021-07-02 16:58:34 +0300

    middleWare checks query from front for validity

--- server/middle/middleWare.js
+++ server/middle/middleWare.js
@@ -1,6 +1,13 @@
-module.exports = {
-    searchQueryValidation: (req, res, next) => {
-        console.log(req.body)
-        next()
-    }
-}
+const middleWare = {
+  searchQueryValidation: (req, res, next) => {
+    middleWare.checkInputValidity(req.body.query)
+      ? next()
+      : res.send({ success: false, error: "Query string is not valid" });
+  },
+  checkInputValidity: (input) => {
+    const pattern = /^[\w\d\s]+$/;
+    return pattern.test(input.trim()) && input.length < 40 ? true : false;
+  },
+};
+
+module.exports = middleWare;

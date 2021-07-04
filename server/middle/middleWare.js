274cc9e6871e22d7589c6b3312e61416363e2b6d
kramwalkis <julius.kramwalkis@gmail.com>
2021-07-04 18:11:33 +0300

    check obj from front is structured like it should

--- server/middle/middleWare.js
+++ server/middle/middleWare.js
@@ -8,6 +8,20 @@ const middleWare = {
     const pattern = /^[\w\d\s]+$/;
     return pattern.test(input.trim()) && input.length < 40 ? true : false;
   },
+  checkArticleValidity: (req, res, next) => {
+    req.body.title &&
+    req.body.description &&
+    req.body.content &&
+    req.body.url &&
+    req.body.image &&
+    req.body.publishedAt &&
+    req.body.source
+      ? next()
+      : res.send({
+          error: true,
+          message: "Data from front is not structured the way expected",
+        });
+  },
 };
 
 module.exports = middleWare;

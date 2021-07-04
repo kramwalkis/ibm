d14b4bf408b854bfa7caf05ccdd54dc698810819
kramwalkis <julius.kramwalkis@gmail.com>
2021-07-04 18:10:44 +0300

    send data to DB from front when article is clicked

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -6,10 +6,25 @@ import Card from "react-bootstrap/Card";
 function Articles({ articles }) {
   function dateConverter(date) {
     const regularDate = new Date(date);
-    let returnDate = `${regularDate.getFullYear()}-${
+    const returnDate = `${regularDate.getFullYear()}-${
       regularDate.getMonth() + 1
     }-${regularDate.getDate()} ${regularDate.getHours()}:${regularDate.getMinutes()}:${regularDate.getSeconds()}`;
     return returnDate;
+  }
+
+  function cardWasClicked(article) {
+    window.open(article.url, "_blank");
+    fetch("/addSearchedArticle", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(article),
+    })
+      .then((req) => req.json())
+      .then((data) => {
+        console.log(data);
+      });
   }
 
   return (
@@ -17,14 +32,19 @@ function Articles({ articles }) {
       {articles.map((article, index) => {
         return (
           <Col key={index} xs={4}>
-            <Card className="m-1">
+            <Card
+              className="m-1 cardStyle"
+              onClick={() => cardWasClicked(article)}
+            >
               <Card.Img variant="top" src={article.image} />
               <Card.Body>
                 <Card.Title>{article.title}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">
                   {dateConverter(article.publishedAt)}
                 </Card.Subtitle>
-                <Card.Text>{article.description}</Card.Text>
+                <Card.Text className="truncate-ellipsis">
+                  {article.description}
+                </Card.Text>
               </Card.Body>
             </Card>
           </Col>
